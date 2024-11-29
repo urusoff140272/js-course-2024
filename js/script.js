83a02a87b16e83d2206b1151918b9d857263f2b5
Andrey Urusov <urusov1402@gmail.com>
2024-11-29 10:02:15 +0200

    bag

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-// проверка на число
-// let x = "9";
-// console.log(Number.isNaN(x));
-// let y = +x;
-// или
-// let z = Number(x);
-// console.log('y=' + typeof y);
-// console.log('z=' + typeof z);
-
-// console.log(!Number.isNaN(y));
-
-
-
 // обьект
 // let user ={};
 
